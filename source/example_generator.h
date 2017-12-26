04f498fbd614847da97e4b06037c8939b972d0ee
Thomas-Greaux <thomas.greaux@etu.unice.fr>
2017-12-26 11:00:54 +0000

    Added a getDistribution

--- source/example_generator.h
+++ source/example_generator.h
@@ -1,6 +1,10 @@
 #ifndef _EXAMPLE_GENERATOR_H
 #define _EXAMPLE_GENERATOR_H
 
+#include <string>
+
+using std::string;
+
 enum Distribution
 {
     Uniform, Geometric, ExtremeValue, Binomial, NegativeBinomial
@@ -12,5 +16,6 @@ private:
     Distribution distribution;
 public:
     ExampleGenerator(Distribution d = Uniform);
+    string get_distribution(void) const;
 };
 #endif
